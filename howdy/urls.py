539465d4662a65cec9e16d15061eba82172cc879
Akash Deep Singhal <akigupta131@gmail.com>
2018-06-07 12:28:52 +0530

    Create Home and About page and render with django

--- howdy/urls.py
+++ howdy/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import url
 from howdy import views
 
 urlpatterns = [
-    url(r'^$', views.HomePageView.as_view())
+    url(r'^$', views.HomePageView.as_view()),
+    url(r'^about/$', views.AboutPageView.as_view()), # Add this /about/ route
 ]
--- howdy/views.py
+++ howdy/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-# Create your views here.
+# Home View.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
+
+# About View.
+class AboutPageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'about.html', context=None)
