8da0d0263df63378ce4f53573b474e54b06c77de
Akash Deep Singhal <akigupta131@gmail.com>
2018-06-07 13:55:43 +0530

    Changed the URL set, added the rest_framework app in settings and minor bug fixes

--- helloapp/settings.py
+++ helloapp/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'howdy'
+    'howdy',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
