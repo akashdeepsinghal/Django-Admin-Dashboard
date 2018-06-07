2269d4558155e7254858fb25d2693f81085ec2be
Akash Deep Singhal <akigupta131@gmail.com>
2018-06-07 15:40:25 +0530

    CreateView class written for generic ListCreateAPIView

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# api/views.py
 
-# Create your views here.
+from rest_framework import generics
+from .serializers import BucketlistSerializer
+from .models import Bucketlist
+
+class CreateView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
+
+    def perform_create(self, serializer):
+        """Save the post data when creating a new bucketlist."""
+        serializer.save()
